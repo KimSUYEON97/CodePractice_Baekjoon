23a26ff0f65cae325bc6f20384ca5ad2a7153499
kim suyeon <calicitine97@naver.com>
2022-04-24 21:59:27 +0900

    [Bronze III] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define plus(t,a,b) printf("Case #%d: %d + %d = %d\n",t,a,b,a+b);
+int main(){
+    int t,a,b;
+    scanf("%d",&t);
+    for(int i=1;i<t+1;i++){
+        scanf("%d %d",&a,&b);
+        plus(i,a,b);
+    }
+    return 0;
+}
