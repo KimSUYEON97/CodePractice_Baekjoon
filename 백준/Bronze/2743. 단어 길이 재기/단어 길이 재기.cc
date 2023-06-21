2369ef0aebfaff0f3e5e1775dec519b9e9fc7dc3
kim suyeon <calicitine97@naver.com>
2023-06-22 01:18:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main(void) {
+	string str;
+	int i;
+	cin>>str;
+	
+	printf("%d",str.length());
+	// your code goes here
+	return 0;
+}
